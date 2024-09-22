32fa67322b0e8a39856e4607ff069a30011bcf4b
MNHACreations <www.mohamed.slash@gmail.com>
2024-09-22 06:35:59 +0300

    Removed unnecessary header.

--- src/Controller/GraphQL.php
+++ src/Controller/GraphQL.php
@@ -172,7 +172,6 @@ class GraphQL
 
         header("Content-Type: application/json; charset=UTF-8");
         header("Access-Control-Allow-Origin: *");
-        header("Access-Control-Allow-Origin: *");
         header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
         header("Access-Control-Allow-Headers: Content-Type, Authorization");
         header("Content-Type: application/json; charset=UTF-8");       

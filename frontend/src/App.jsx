394f795eb56986144820c2759599dc2892310a6d
MNHACreations <www.mohamed.slash@gmail.com>
2024-10-06 04:29:08 +0300

    Logic change of handleAddProduct, reformatted & added logic of screen dimmer

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,7 +4,7 @@ import "./App.css";
 import NavBar from './components/NavBar.jsx'
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import MainLayout from './layouts/MainLayout.jsx'
-import {gql} from '@apollo/client'
+import { gql } from '@apollo/client'
 import { Query } from "@apollo/client/react/components";
 import Products from "./components/Products.jsx";
 import Cart from "./components/Cart.jsx";
@@ -13,60 +13,80 @@ import productScreenWithRouter from './screens/ProductScreen.jsx';
 
 
 export default class App extends React.Component {
-constructor(props){
-    super(props);
+    constructor(props) {
+        super(props);
+        this.state = {
+            isCartReady: false,
+            isDimmed: false
+        }
+        this.cartRef = React.createRef();
+    }
 
-    this.cartRef = React.createRef();
-}
-GET_CATEGORIES = gql`query {
+    GET_CATEGORIES = gql`query {
     products {
         category
     }
 }`;
-handleAddProduct = (product) => {
-    this.cartRef.current.addProduct(product);
-};
- getUniqueCategories(data) {
-    const uniqueCategories = [];
+    handleAddProduct = (product) => {
+        this.cartRef.current.addProduct(product);
+    };
 
-    data.products.forEach(product => {
-        if (!uniqueCategories.includes(product.category)) {
-            uniqueCategories.push(product.category);
-        }
-    });
+    toggleDim = () => {
+        this.setState({isDimmed: !this.state.isDimmed});
+    };
+    getUniqueCategories(data) {
+        const uniqueCategories = [];
 
-    return uniqueCategories;
-}
-  render() {
-    return (
+        data.products.forEach(product => {
+            if (!uniqueCategories.includes(product.category)) {
+                uniqueCategories.push(product.category);
+            }
+        });
+
+        return uniqueCategories;
+    }
+    isCartReady = () => {
+        return this.state.isCartReady;
+    }
+    setIsCartReady = (bool) => {
+        this.setState({ isCartReady: bool });
+    }
+    // DEBUGGING
+    componentDidMount() {
+    };
+
+    render() {
+
+        return (
 
 
-        <Query query={this.GET_CATEGORIES}>
-        {({loading, error, data}) => {
-       if (data) {
+            <Query query={this.GET_CATEGORIES}>
+                {({ loading, error, data }) => {
+                    if (data) {
 
-        
-            const uniqueCategories = this.getUniqueCategories(data); 
-            const firstCategory = uniqueCategories[0];
-            return <BrowserRouter>
 
-            <Cart ref={this.cartRef} /> 
-               <MainLayout />
-            <Routes>
-                {/*Define static routes, followed by dynamic ones.*/}
-               <Route path={`/product/:id`} Component={ProductScreen}/>                
-               <Route key={firstCategory} index element={<Products category={firstCategory} onProductAdd={this.handleAddProduct}/>}/>
-               return {uniqueCategories.map(category => <Route key={category} path={`/${category}`}
-                   element={<Products category={category} onProductAdd={this.handleAddProduct} />}
-                   shouldRevalidate={true}
-                   />)};
+                        const uniqueCategories = this.getUniqueCategories(data);
+                        const firstCategory = uniqueCategories[0];
+                        return <BrowserRouter>
 
-            
-            </Routes>
-        </BrowserRouter> 
-       } 
-       }}
-        </Query>
-    );
-  }
+                            <Cart setIsCartReady={this.setIsCartReady} ref={this.cartRef} />
+                            {this.isCartReady() && <MainLayout cartRef={this.cartRef} />}
+
+                            <Routes>
+                                {/*Define static routes, followed by dynamic ones.*/}
+                                <Route path={`/product/:id`} element={<ProductScreen cartRef={this.cartRef} />} />
+                                <Route key={firstCategory} index element={<Products category={firstCategory} onProductAdd={this.handleAddProduct} />} />
+                                return {uniqueCategories.map(category => <Route key={category} path={`/${category}`}
+                                    element={<Products category={category} onProductAdd={this.handleAddProduct} />}
+                                    shouldRevalidate={true}
+                                />)};
+
+
+                            </Routes>
+                        </BrowserRouter>
+                    }
+                }}
+            </Query>
+        );
+    }
 }

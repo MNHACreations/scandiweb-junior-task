1847fd6a18427d07b21e85b57d1f6f4d3b9c1269
MNHACreations <www.mohamed.slash@gmail.com>
2024-10-06 04:22:27 +0300

    Added tailwind css (To be used in some areas for faster development)

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,19 @@
+import { defaults } from 'autoprefixer';
+
+/** @type {import('tailwindcss').Config} */
+export default {
+ corePlugins: {
+    preflight: false,
+  },
+  content: ['./src/**/*.{html,js,jsx}'],
+  theme: {
+    extend: {},
+     
+
+  },
+  
+    
+  plugins: [],
+    
+}
+ 

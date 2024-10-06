90d50d673551e36cf908cb075a190eb1eb3b080e
MNHACreations <www.mohamed.slash@gmail.com>
2024-10-06 04:32:11 +0300

    Giving cartRef as a prop to NavBar

--- frontend/src/layouts/MainLayout.jsx
+++ frontend/src/layouts/MainLayout.jsx
@@ -1,13 +1,21 @@
-import React from 'react'
+import React, { createRef } from 'react'
 import NavBar from '../components/NavBar.jsx';
-
+import PropTypes from 'prop-types'
 export default class MainLayout extends React.Component{
-
-
+    constructor(props) {
+        super(props);
+    }
+    
     render() {
         return (
-        <NavBar /> 
+            <NavBar cartRef={this.props.cartRef} /> 
 
         );
     }
+
+
 }
+
+MainLayout.PropTypes = {
+    getCartItems: PropTypes.func
+};
